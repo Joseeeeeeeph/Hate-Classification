95ee873d80da1b8744ddc93d02320c971c7f8f24
Joseph Adams <josephadams2@virginmedia.com>
2024-08-27 15:08:04 +0100

    Added new dataset and major restructuring 4

---
+++ src/scraper.py
@@ -0,0 +1,23 @@
+import os
+import requests
+import pandas as pd
+
+dataset_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data/avaapm-hatespeech/dataset_v2')
+storage_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data/avaapm-hatespeech/tweetdata')
+
+csv = pd.read_csv(os.path.join(dataset_path, 'hate_speech_dataset_v2.csv'), usecols=['TweetID', 'LangID', 'HateLabel'])
+csv_en = csv[csv['LangID'] == 1]
+ids = csv_en['TweetID'].values.tolist()
+
+for id in ids:
+    url = f"https://cdn.syndication.twimg.com/tweet-result?id={id}&token=a"
+
+    try:
+        text = requests.get(url).json()['text']
+        file = open(os.path.join(storage_path, f'{id}.txt'), 'w')
+        file.write(text)
+        print(f"Getting tweet {id}: success")
+    except:
+        print(f"Getting tweet {id}: fail")
+
+print(f'\nTweet contents stored in {storage_path}')
